3a72df0e3d12a999c906e806b13af3fe789c9334
odero11 <97958881+odero11@users.noreply.github.com>
2022-03-15 14:20:23 +0300

    Add files via upload

---
+++ winnie library.c
@@ -0,0 +1,35 @@
+\*
+county library management system
+by winnie odero
+feb 2022
+mit lincense
+c89 complier
+*\
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("county library system!\n");
+    printf("welcome winnie odero !\n");
+    printf("you selected action %d",menu());
+    return 0;
+}
+int menu(){
+int action;
+printf("select an action:\n");
+printf("1.add a new patron\n");
+printf("2.view patron\n");
+printf("3.view books\n");
+printf("4.add new books\n");
+printf("your action:\n");
+scanf["%d",&action];
+return action;
+}
+
+
+
+
+
+
+
